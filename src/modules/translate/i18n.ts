273af84c807d6b147051c94a01ec819e63834cd3
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2025-01-31 12:33:51 -0300

    :sparkles: Add LanguageDetector

--- src/modules/translate/i18n.ts
+++ src/modules/translate/i18n.ts
@@ -1,17 +1,39 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { messages } from "./languages";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18n.use(initReactI18next).init({
-  resources: messages,
-  fallbackLng: "pt",
-  defaultNS: ["translation"],
-  ns: ["translation"],
-  lng: "pt",
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+// const resources = {
+//   en: {
+//     translation: {
+//       homePageName: "Home",
+//     },
+//   },
+//   pt: {
+//     translation: {
+//       homePageName: "Início",
+//     },
+//   },
+// };
 
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .init({
+    resources: messages,
+    fallbackLng: "pt",
+    defaultNS: ["translation"],
+    ns: ["translation"],
+    lng: "pt", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+    // if you're using a language detector, do not define the lng option
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
 
 export default i18n;
